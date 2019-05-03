11345b83921566f3dcc08b217a067cf087b7fa44
Dani Castilho <danicastilhodev@gmail.com>
2019-05-03 08:44:31 -0300

    Inicia modificações de JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Script que controla a exibição dos subitens do item eventos do menú
+
 var showMenu = document.querySelector('#showMenu')
 var submenu = document.querySelector('.submenu')
 
@@ -9,3 +11,35 @@ showMenu.addEventListener('mouseleave', function(event) {
 	submenu.classList.remove("active")
 })
 
+//script que controla a exibição dos ingredientes dos pratos na home
+
+/* 
+	para começar, selecionamos todos os elementos <figure> 
+	através do document.querySelectorAll.
+*/
+	var galeria = document.querySelector('.galeria')
+	var pratos = galeria.querySelectorAll('figure');
+
+ /*
+	Como temos mais de 1 elemento figure na forma, teremos como retorno, uma lista, 
+	ou seja, um array de elementos. 
+
+	Agora precisamos iterar, ou seja, passar por todos esses elementos.
+	Para isso, iremos usar o laço de repetição 'for'.
+*/
+
+	for(var i = 0; i <= pratos.length; i++) {
+		pratos[i].addEventListener('mouseover', function(event) {
+			this.classList.add('active')
+		})
+
+		pratos[i].addEventListener('mouseleave', function(event) {
+			this.classList.remove('active')
+		})
+	}
+/*
+	
+	e adicionar um 'ouvidor de eventos'. No Javascript, esse ouvidor se chama 'Event Listener', 
+	e o méto usado para adicionar esse ouvidor ao elemento, chama-se 'addEventListener'.
+	
+ */
